2124b6e875dd3ca9385e1173d6f3aef1e28fc152
nicholas-schaefere <nschaefe@optonline.net>
2019-08-05 09:46:05 -0400

    installed axios package to manage web api get requests

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+var axios = require("axios");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
